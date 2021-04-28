be673b78f580c97e5286aaa6aca8df68a20f9127
Николай Вокса <@bkp.local>
2021-04-28 16:20:58 +0300

    init expo

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from "react";
+import { Animated, Text, View, Button } from "react-native";
+import styled from "styled-components/native";
 
 export default function App() {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const fadeIn = () => {
+    // Will change fadeAnim value to 1 in 2 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    // Will change fadeAnim value to 0 in 1 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 700,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Animated.View style={{ opacity: fadeAnim }}>
+        <GroupTitle> tubus </GroupTitle>
+      </Animated.View>
+      <Button title="Fade_In" onPress={fadeIn} />
+      <Button title="FadeOuT-" onPress={fadeOut} />
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #0a4f0a;
+`;
+
+const GroupTitle = styled.Text`
+  font-size: 40px;
+  font-weight: 600;
+  color: #ffffff;
+`;
