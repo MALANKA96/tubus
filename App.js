b5370c000ab86f1b1512588449ddade5672686d5
Николай Вокса <@bkp.local>
2021-04-29 16:04:46 +0300

    add input task

--- App.js
+++ App.js
@@ -1,47 +1,85 @@
 import React, { useRef } from "react";
-import { Animated, Text, View, Button } from "react-native";
+import {
+  Animated,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
 import styled from "styled-components/native";
 
 export default function App() {
-  const fadeAnim = useRef(new Animated.Value(0)).current;
-  const fadeIn = () => {
-    // Will change fadeAnim value to 1 in 2 seconds
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start();
-  };
-
-  const fadeOut = () => {
-    // Will change fadeAnim value to 0 in 1 seconds
-    Animated.timing(fadeAnim, {
-      toValue: 0,
-      duration: 700,
-      useNativeDriver: true,
-    }).start();
-  };
-
   return (
     <Container>
-      <Animated.View style={{ opacity: fadeAnim }}>
-        <GroupTitle> tubus </GroupTitle>
-      </Animated.View>
-      <Button title="Fade_In" onPress={fadeIn} />
-      <Button title="FadeOuT-" onPress={fadeOut} />
+      <Header>
+        <NameApp onPress={() => Alert.alert("application TODOlist")}>
+          tubus
+        </NameApp>
+      </Header>
+      <InputTodo>
+        <TextInputTodo placeholder="write text task..." />
+        <ButtonAddTodo>
+          <TextButton onPress={() => Alert.alert("add task")}>ADD</TextButton>
+        </ButtonAddTodo>
+      </InputTodo>
+      <Middle>
+        <TitleTask>next task</TitleTask>
+      </Middle>
     </Container>
   );
 }
 
 const Container = styled.View`
+  margin-top: 24px;
   flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: #0a4f0a;
+  background-color: #c7c4c1;
+`;
+const Header = styled.View`
+  background-color: #3f51b5;
 `;
 
-const GroupTitle = styled.Text`
-  font-size: 40px;
-  font-weight: 600;
-  color: #ffffff;
+const NameApp = styled.Text`
+  text-align: left;
+  font-family: Roboto;
+  font-size: 50px;
+  font-weight: 800;
+  color: #c7c4c1;
+`;
+const InputTodo = styled.View`
+  margin-top: 5px;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const TextInputTodo = styled.TextInput`
+  width: 85%;
+  font-family: Roboto;
+  font-size: 24px;
+  background-color: #c7c4c1;
+  border: 2px #3f51b5;
+`;
+
+const TextButton = styled.Text`
+  color: #c7c4c1;
+  font-family: Roboto;
+  font-size: 24px;
+  text-align: center;
+`;
+const ButtonAddTodo = styled.TouchableOpacity`
+  width: 15%;
+  background-color: #3f51b5;
+`;
+
+const Middle = styled.View`
+  margin-top: 5px;
+  background-color: #3f51b5;
+`;
+
+const TitleTask = styled.Text`
+  text-align: center;
+  font-family: Roboto;
+  font-size: 24px;
+  color: #c7c4c1;
 `;
